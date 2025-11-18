39e7ec0edaa9c51ab565a766afaa34d7b8c6fb07
Matheus Carvalho <matheusscarvlh@gmail.com>
2025-11-17 21:26:59 -0300

    Commit 2

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,68 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Home() {
 
-    const [gastos, setGastos] = useState([]);
+    const [movimentos, setMovimentos] = useState([]);
 
     const [nome, setNome] = useState("");
     const [desc, setDesc] = useState("");
     const [valor, setValor] = useState("");
     const [data, setData] = useState("");
 
-    function enviarMovimento() {
-        const gasto = {
+    function adicionarMovimento() {
+        const novoMovimento = {
             nome: nome,
-            descricao: desc,
+            desc: desc,
             valor: Number(valor),
             data: data,
+            tipo: "despesa",
         };
-
-        setGastos(prev => {
-            const atualizado = [...prev, gasto];
-            console.log(atualizado);
-            return atualizado
-        });
+        setMovimentos([...movimentos, novoMovimento]);
     };
 
-    const total = gastos.reduce((acc, item) => acc + item.valor, 0);
+    // USE EFFECT - MOSTRAR VALOR
+    useEffect(() => {
+        console.log(movimentos);
+    }, [movimentos]);
+
+    // DESPESA
+    const despesa = movimentos
+        .filter(mov => mov.tipo === "despesa")
+        .reduce((acc, mov) => acc + mov.valor, 0);
+
+    // RECEITA
+    const receita = movimentos
+        .filter(mov => mov.tipo === "receita")
+        .reduce((acc, mov) => acc + mov.valor, 0);
+
+    // TOTAL
+    const total = movimentos.reduce((acc, mov) => acc + mov.valor, 0);
 
     return (
         <div>
             <header>
-                <h1>Controle de Valores</h1>
+                <h1>Controle Financeiro</h1>
             </header>
             <main>
                 <div>
-                    <h2>Movimentacoes</h2>
-                    <div>
-                        <p>Total Despesas {total}</p>
-                        <p>Total Receitas</p>
-                    </div>
-                    <div>
-                        <h3>Lista</h3>
-                        <ul>
-                            {gastos.map((m, i) => (
-                                <li key={i}>
-                                    {m.nome} - R$ {m.valor} - {m.data}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                    <h2>Movimentos</h2>
+                    <p>Despesas {despesa}</p>
+                    <p>Receitas {receita}</p>
+                    <p>Valor Final {total}</p>
+                    <ul>
+                        {movimentos.map((mov, index) => (
+                            <li key={index}>
+                                {mov.nome} - R$ {mov.valor} - {mov.data}
+                            </li> 
+                        ))}
+                    </ul>
                 </div>
-                <h2>Insira sua Movimentação</h2>
                 <div>
-                    <input type="text" placeholder="Nome" value={nome} 
-                    onChange={(e) => setNome(e.target.value)}/>
-
-                    <input type="text" placeholder="Descricao" value={desc}
-                    onChange={(e) => setDesc(e.target.value)}/>
-
-                    <input type="number" placeholder="Valor" value={valor} 
-                    onChange={(e) => setValor(e.target.value)}/>
-
+                    <h2>Adicionar Movimento</h2>
+                    <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)}/>
+                    <input type="text" placeholder="descrição" value={desc} onChange={(e) => setDesc(e.target.value)}/>
+                    <input type="number" placeholder="Valor" value={valor} onChange={(e) => setValor(e.target.value)}/>
                     <input type="date" value={data} onChange={(e) => setData(e.target.value)}/>
-
-                    <button type="button" onClick={enviarMovimento}>Adicionar</button>
+                    <button type="button" onClick={adicionarMovimento}>Adicionar</button>
                 </div>
             </main>
         </div>
